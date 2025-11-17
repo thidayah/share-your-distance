bfd6865f769769ddaf3318f577607e28b207e5cd
taufik <taufik@codelabs.co.id>
2025-11-17 16:53:18 +0700

    feat: init supabase client and server config

---
+++ src/lib/supabase/server-client.ts
@@ -0,0 +1,13 @@
+import { createClient } from '@supabase/supabase-js';
+
+const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL!;
+const supabaseServiceKey = process.env.SUPABASE_SERVICE_ROLE_KEY!;
+
+export const supabaseServer = createClient(supabaseUrl, supabaseServiceKey, {
+  auth: {
+    persistSession: false, // server tidak perlu session
+  },
+  db: {
+    schema: 'public',
+  },
+});
