0f13eb94e234e97d4ac8814010d9c3027f84a9e3
taufik <taufik@codelabs.co.id>
2025-12-06 06:28:44 +0700

    feat: list contact messages

--- src/lib/supabase/service/contact-messages/types.ts
+++ src/lib/supabase/service/contact-messages/types.ts
@@ -4,6 +4,7 @@ export interface ContactMessagesCreate {
   subject: string;
   category: string;
   message: string;
+  is_read?: boolean;
 }
 
 export interface ContactMessagesUpdate extends Partial<ContactMessagesCreate> {
